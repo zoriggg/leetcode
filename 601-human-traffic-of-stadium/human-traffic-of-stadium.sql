66f12c69ca11784ea4c17f5cf239def00f544502
zoriggg <74913482+zoriggg@users.noreply.github.com>
2025-02-16 22:31:12 +0800

    Time: 363 ms (45.87%) | Memory: 0B (100.00%) - LeetSync

---
+++ 601-human-traffic-of-stadium/human-traffic-of-stadium.sql
@@ -0,0 +1,42 @@
+select
+    t.id,
+    t.visit_date,
+    t.people
+from
+    (    
+        select 
+            row_number() over(order by s.id) as row_n,
+            s.id,
+            s.visit_date,
+            s.people
+        from
+            Stadium s 
+        where
+            s.people >= 100
+    ) as t
+where
+    (t.id - row_n) in 
+    (
+        select
+            tt.nn
+        from
+            (
+                select
+                    t.id,
+                    t.id - row_n as nn
+                from
+                (    
+                    select 
+                        row_number() over(order by s.id) as row_n,
+                        s.id
+                    from
+                        Stadium s 
+                    where
+                        s.people >= 100
+                ) as t
+            ) as tt
+        group by
+            tt.nn
+        having 
+            count(id) >= 3
+    )
